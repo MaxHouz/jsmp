daaae21c93897a8d14501cbb029e38ed9a782f31
Maksym_Shatailo <maksym_shatailo@epam.com>
2019-07-30 18:26:22 +0300

    task9: node.js task init, added log for device actions

---
+++ 09-nodejs/frontend/src/api.js
@@ -0,0 +1,57 @@
+import axios from 'axios';
+
+const API_URL = 'http://localhost:4000';
+
+
+export async function getDevices() {
+    const response = await axios.get(`${API_URL}/devices`);
+    return response.data;
+}
+
+export async function getDeviceById(deviceId) {
+    const response = await axios.get(`${API_URL}/devices/${deviceId}`);
+    return response.data;
+}
+
+export async function addDevice(device) {
+    const response = await axios.post(`${API_URL}/devices`, device);
+    return response.data;
+}
+
+export async function removeDevice(deviceId) {
+    await axios.delete(`${API_URL}/devices/${deviceId}`);
+}
+
+export async function updateDevice(deviceId, data) {
+    await axios.patch(`${API_URL}/devices/${deviceId}`, data);
+}
+
+export async function switchOn(deviceId) {
+    await updateDevice(deviceId, {
+        state: 'on',
+        log: {
+            date: getFormattedDate(),
+            action: 'on'
+        }
+    });
+}
+
+export async function switchOff(deviceId) {
+    await updateDevice(deviceId, {
+        state: 'off',
+        log: {
+            date: getFormattedDate(),
+            action: 'off'
+        }
+    });
+}
+
+export async function getDeviceLog(deviceId) {
+    const response = await axios.get(`${API_URL}/devices/log/${deviceId}`);
+    return response.data;
+}
+
+function getFormattedDate() {
+    let date = new Date();
+    return `${date.getFullYear()}-${(date.getMonth() + 1)}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+}
---
+++ 09-nodejs/server/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const corsMiddleware = require('./middlewares/core');
+const devicesRouter = require('./routes/devices');
+
+
+mongoose.connect('mongodb://localhost:27017/node-jsmp', {useNewUrlParser: true});
+
+
+const app = express();
+const PORT = 4000;
+
+app.use(express.json());
+app.use(corsMiddleware);
+app.use('/devices', devicesRouter);
+
+app.get('/', (req, res) => {
+    res.json({result: 'ok 1'});
+});
+
+app.listen(PORT, () => {
+    console.log(`server is listening port on http://localhost:${PORT}`);
+});
